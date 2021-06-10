74eb730cb0c70fffa44e526f2188dc1256f27991
Krishanth <78636188+ChomperThePug@users.noreply.github.com>
2021-06-10 16:41:26 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,139 +1,78 @@
-var block1, block2, block3, block4, block5;
-var Pblock;
-var Invisiblock1, Invisiblock2, Invisiblock3;
-var border1, border2, border3, border4;
-var trophy, trophyI;
-var speed = 10;
-var timer = 0;
-var Message = "";
-//Game State Values
-var START = 0;
-var FOOL = 1;
-var REALIZE = 2;
-var MOVE = 3;
-var WIN = 4;
-var gameState = START;
-//Trophy State Values
-var DEFAULT = 0;
-var HELD = 1;
-var trophyState = DEFAULT;
+var Button ;
+var edges;
+var count = 10;
+var textState = 0;
+var gameState = 0;
+var Background;
+var speechState = 0;
+var speechCount = 0;
 
 function preload() {
-  trophyI = loadImage("../Assets/trophy.png");
+  Background = loadImage("Assets/Background.jfif");
 }
 
 function setup() {
   createCanvas(700, 700);
-  //Maze Blocks
-  block1 = createSprite(240, 200, 300, 30);
-  block2 = createSprite(460, 358, 30, 350);
-  block3 = createSprite(80, 335, 30, 300);
-  block4 = createSprite(215, 480, 300, 30);
-  block5 = createSprite(320, 335, 250, 30);
-  //Player Block
-  Pblock = createSprite(417.5, 200, 35, 35);
-  Pblock.shapeColor = "red";
-  //Invisiblock
-  Invisiblock1 = createSprite(350, 400, 30, 150);
-  Invisiblock1.visible = false;
-  Invisiblock2 = createSprite(400, 170, 150, 30);
-  Invisiblock2.visible = false;
-  Invisiblock3 = createSprite(630, 370, 340, 30);
-  Invisiblock3.visible = false;
-  //Trophy
-  trophy = createSprite(405, 500, 30, 30)
-  trophy.addImage(trophyI);
-  trophy.scale = 0.5; 
-  //Borders
-  border1 = createSprite(width / 2, height, width, 20);
-  border2 = createSprite(width / 2, 0, width, 20);
-  border3 = createSprite(0, height / 2, 20, height);
-  border4 = createSprite(width, height / 2, 20, height);
-  //Border Visibility
-  border1.visible = false;
-  border2.visible = false;
-  border3.visible = false;
-  border4.visible = false;
+  edges = createEdgeSprites();
+  Button = createSprite(350, 350, 200, 50);
+
 }
 
 function draw() {
-  background(0, 255, 255);
-  //Controls
-  if (keyDown("Up")) {
-    Pblock.velocityY = -speed;
-  } else if (keyDown("Down")) {
-    Pblock.velocityY = speed;
-  } else {
-    Pblock.velocityY = 0;
+  background(Background);
+  Button.collide(edges);
+  console.log(speechState);
+  //Game State 0
+  if (gameState === 0) {
+    if (textState === 1) {
+      let a = 0;
+      a += 2 ;
+      if(frameCount % count === 0){
+        count = Math.round(random(20,30));
+        var buttonPosX = Math.round(random(0,700));
+        var buttonPosY = Math.round(random(0,700));
+        Button.x = buttonPosX;
+        Button.y = buttonPosY;
+      }
+      if (mousePressedOver(Button) && a !== 0) {
+        gameState = 1;
+        Button.destroy();
+      }
+    }
+    if (mousePressedOver(Button)) {
+      textState = 1
+       Button.x = Math.round(random(0,700));
+       Button.y = Math.round(random(0,700));
+    }
+  }else if //Game State 1
+  (gameState === 1) {
+    speech(0,"Hello There", 100)
   }
-  if (keyDown("Right")) {
-    Pblock.velocityX = speed
-  } else if (keyDown("Left")) {
-    Pblock.velocityX = -speed
-  } else {
-    Pblock.velocityX = 0;
-  }
-  //Collisions
-  Pblock.collide(border1);
-  Pblock.collide(border2);
-  Pblock.collide(border3);
-  Pblock.collide(border4);
-  Pblock.collide(block1);
-  Pblock.collide(block2);
-  Pblock.collide(block3);
-  Pblock.collide(block4);
-  Pblock.collide(block5);
-  Pblock.collide(Invisiblock1);
-  trophy.collide(block1);
-  trophy.collide(block2);
-  trophy.collide(block3);
-  trophy.collide(block4);
-  trophy.collide(block5);
-  trophy.collide(Invisiblock1);
-  //Speech
-  textSize(50);
-  textAlign(CENTER);
-  text(Message, 350, 100);
-  //Game States
-  if (gameState == START) {
-    Pblock.collide(Invisiblock2);
-    //Invisiblock1 Activation
-    if (intersect(Pblock, Invisiblock1)) {
-      gameState = FOOL
-    }
-  } else if (gameState == FOOL) {
-	Invisiblock1.visible = true;
-    if (intersect(Pblock, Invisiblock2)) {
-      gameState = REALIZE
-    }
-  } else if (gameState == REALIZE) {
-    if (intersect(Pblock, Invisiblock3)) {
-      gameState = MOVE
-    }
-  } else if (gameState == MOVE) {
-    //Winning
-    if ((proximity(Pblock, trophy, -35) && trophyState == HELD)) {
-      gameState = WIN ; 
-      console.log("You Win!");
-    }
-    //Trophy State
-    if (mousePressedOver(trophy)) {
-      trophyState = HELD;
-    } else {
-      trophyState = DEFAULT
-    }
-    //Proximity
-    if (proximity(Pblock, trophy, 50) && trophyState == DEFAULT) {
-      trophy.x = Math.round(random(100,600))
-      trophy.y = Math.round(random(100, 600))
-    }
-    //Holding The Trophy
-    if (trophyState == HELD) {
-      trophy.x = mouseX;
-      trophy.y = mouseY;
-    }
-  } else if(gameState = WIN){
-    Message = "You Win";
-  }
+ // console.log(gameState)
   drawSprites();
+  if (gameState === 0) {
+    textAlign(CENTER);
+    textSize(20);
+    fill("White");
+    text("Do Not Press To Start", Button.x,Button.y + 8);
+  }
+}
+
+//Speech Function
+function speech(state,message,timer) {
+  let a = 0;
+ 
+  if (speechState === state) {
+    speechCount++ ;
+    if (speechCount < timer + 1) {
+      textAlign(CENTER);
+      textSize(20);
+      fill('white');
+      text(message, width/2, 30);
+    }
+    if (speechCount === timer) {
+      speechState = (state + 1) ;
+    }
+    console.log(speechCount);
+  }
 }
